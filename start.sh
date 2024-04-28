4e29eeea04b33b34d8425613ae41d46307ca679d
David Reis <post@d-reis.com>
2024-04-28 13:26:33 +0200

    Adjust startup to allow for clean setup

--- start.sh
+++ start.sh
@@ -12,6 +12,25 @@ fi
 _log "Activating virtual environment"
 source .venv/bin/activate
 
+if [ ! -f .env ]; then
+    _log "Creating .env file"
+    cp .env.example .env
+fi
+
+if [ ! -d workspace ]; then
+    _log "Creating workspace directory"
+    ./reset.sh
+fi
+
+set -a
+source .env
+set +a
+
+if [ "${LLAMA_MODEL_PATH:-undefined}" = "undefined" ]; then
+    _log "Configure your model path in .env!"
+    exit 1
+fi
+
 _log "Installing dependencies"
 poetry install
 
